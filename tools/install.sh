84cf68053ea4df16220ca6c9732c744b02a2ebce
zhiqi.zhang <zhiqi.zhang@yitu-inc.com>
2023-05-17 16:55:56 +0800

    update install scripts

--- tools/install.sh
+++ tools/install.sh
@@ -67,7 +67,7 @@ zdot="${ZDOTDIR:-$HOME}"
 ZSH="${ZSH:-$HOME/.oh-my-zsh}"
 
 # Default settings
-REPO=${REPO:-ohmyzsh/ohmyzsh}
+REPO=${REPO:-zhiqizhiqi/ohmyzsh}
 REMOTE=${REMOTE:-https://github.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 
